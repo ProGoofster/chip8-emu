136651db3660581f206d76195a5754fb8b81a2d0
Jake Berryman <59934842+ProGoofster@users.noreply.github.com>
2025-02-13 00:34:41 -0600

    refactor: restructure project and add initial emulator setup

--- chip8-core/src/lib.rs
+++ chip8-core/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//screen size.
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+//Chip-8 doesn't have a set amount of memory, but 4KiB is what it was designed for.
+const RAM_SIZE: usize = 4096;
+const NUM_REGS: usize = 16;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+//emulator data
+pub struct emu {
+    pc: u16,
+    ram: [u8; RAM_SIZE],
+    screen: [[bool; SCREEN_WIDTH]; SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGS],
+    i_reg: u16,
 }
